e46ea273de082f026fdc16fbd0708774ad53e09d
EDTtoLove <37850087+EDTtoLove@users.noreply.github.com>
2018-03-28 00:03:47 +0300

    Add files via upload

--- Test.java
+++ Test.java
@@ -1,3 +1,5 @@
+package Word;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -10,17 +12,35 @@ import java.util.stream.Stream;
 
 public class Test {
     public static void main(String[] args) throws IOException {
-     BufferedReader reader = new BufferedReader(new InputStreamReader(System.in,"utf-8"));
-     String str ="";
-     try{
-         str = reader.readLine();
-     }catch (IOException e){
-         e.printStackTrace();
-     }
-        String t = MCrypto.Encrypt(str);
-        System.out.println(t);
-        System.out.println(MCrypto.Decrypt(t));
+        System.out.println("                                                                                                         \n" +
+                "               @@@                        @5                                                     @@           \n" +
+                " @@@@@         @@@               @@@@@@@  @@                                  @@@@@              @@           \n" +
+                " @@     @@@@X  @@@ @@@  @@@@     @@@  @@  @@  @@r@@s  @@@@@   @@@@@@@  @@    @@;      @@@@    @@@@@   @@@@    \n" +
+                " @@@@@     @@@ @@@@@@  @@  @@    @@@@@@   @@  @@@ @@  ,  @@@  @@@@ @@ ;@@    @@      @@  @@  @@  @@  @@  @@   \n" +
+                " @@     @@@@@@ @@@@@   @@@@@@    @@@  @@  @@  @@  @@  @@@@@@  @@   X@@@@     @@     @@@  @@ X@@  @@  @@@@@@   \n" +
+                " @@    @@@ @@@ @@@ @@  @@        @@@  @@  @@  @@  @@  @@  @@  @@    @@@@     @@@;    @@  @@  @@  @@  @@       \n" +
+                " @@     @@@ @@ @@@ @@@  @@@@@    @@@@@@   @@  @@  @@  @@@@@@  @@     @@        @@@@5  @@@@    @@@@@   @@@@@   \n" +
+                "                                                                   @@@                                        \n" +
+                "                                                                   @r                                      ");
 
+            System.out.println("Введите текст для шифровки/расшифровки");
+            System.out.print("> ");
+            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in, "utf-8"));
+
+            String str = "";
+            String strWord = "";
+            try {
+                str = reader.readLine();
+                // strWord = str.replace(' ', '¶');
+                // System.out.println(strWord);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            String t = MCrypto.Encrypt(str);
+            System.out.println(t);
+            //  System.out.println(MCrypto.Decrypt(t));
+
+        }
     }
-}
+
 
